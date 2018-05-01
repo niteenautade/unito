1b84bc9e631871fff3dec8ab2417ceebd35a9136
niteenautade <niteenautade@gmail.com>
2018-05-01 11:37:48 +0530

    sort,projection fix

--- middlewares/blueprints.js
+++ middlewares/blueprints.js
@@ -16,7 +16,7 @@ module.exports = function(){
     var blueprints = {
         find : function(req,res,next){
             let modelName = services.modelName(req)
-            let paramsWithoutKeywords = removeKeywords(req.Params) //limit,populate
+            let paramsWithoutKeywords = removeKeywords(req.Params) //limit,populate,projection,sort
             var query = services.Api[modelName].find({...paramsWithoutKeywords})
             operations(query,req.Params)
             .then((data)=>{
@@ -80,16 +80,43 @@ function operations(query,params) {
     return Promise.resolve()
     .then(()=>{
         if(params.projection){
-            if (!isStringObject(params.projection)) {
+            if(isStringObject(params.projection)){
+                if(isObjectArray(params.projection)){
+                    if(Array.isArray(params.projection)){
+                        query = query.select(params.projection.join(" "))                        
+                    }
+                }
+                else{
+                    query = query.select(JSON.parse(params.projection))
+                }
+            }
+            else{
                 query = query.select(params.projection)
-            } else {
-                query = query.select(JSON.parse(params.projection))                
             }
         }
     })
     .then(()=>{
         if(params.limit){
             query = query.limit(parseInt(params.limit))
+        }
+    })
+    .then(()=>{
+        if(params.sort){
+            if(isStringObject(params.sort)){
+                if(isObjectArray(params.sort)){
+                    if(typeof(params.sort)=="string"){
+                        query = query.sort(JSON.parse(params.sort).toString())
+                    }else if(Array.isArray(params.sort)){
+                        query = query.sort(params.sort.join(" "))                        
+                    }
+                }
+                else{
+                    query = query.sort(JSON.parse(params.sort))
+                }
+            }
+            else{
+                query = query.sort(params.sort)
+            }
         }
     })
     .then(()=>{
@@ -106,16 +133,34 @@ function operations(query,params) {
 function removeKeywords(params){
     var copy = _.cloneDeep(params)
     delete copy.limit
+    delete copy.sort
     delete copy.populate
     delete copy.projection
     return copy
 }   
 
 function isStringObject(str) {
-    try {
-        JSON.parse(str);
-    } catch (e) {
-        return false;
+    str = JSON.stringify(str);
+    str = JSON.parse(str);console.log(str,typeof(str))
+    if(typeof(str)=="string"){
+        try {
+            str = JSON.parse(str)
+        } catch (e) {
+            return false            
+        }
     }
-    return true;
+    return true
+}
+
+function isObjectArray(str) {
+    str = JSON.stringify(str);
+    str = JSON.parse(str);
+    if(typeof(str)=="string"){
+        try {
+            str = JSON.parse(str)
+        } catch (e) {
+            return false            
+        }
+    }
+    return Array.isArray(str);
 }
