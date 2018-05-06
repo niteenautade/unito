559f7956df27bcca6836b44ef242c3241df8cc41
niteenautade <niteenautade@gmail.com>
2018-05-06 18:35:07 +0530

    params merged into where refactor

--- blueprints/find.js
+++ blueprints/find.js
@@ -1,5 +1,5 @@
 var _ = require("lodash")
-var operations = require('./cursorOperations')
+var cursorOperations = require('./cursorOperations')
 var removeKeywords = require('./utils/removeKeywords')
 var services = require('require-all')({
 	dirname     :  __dirname+'/../services',
@@ -11,21 +11,13 @@ module.exports = function(options){
     return function(req,res,next){
         let modelName = services.modelName(req);
         let reqParamsCopy = _.cloneDeep(req.Params)
-        if(reqParamsCopy && reqParamsCopy.where){
-            reqParamsCopy.where = JSON.parse(reqParamsCopy.where)
-            if(reqParamsCopy.populate && reqParamsCopy.where.populate){
-                if(typeof(reqParamsCopy.populate)=="string" && reqParamsCopy.populate != reqParamsCopy.where.populate){
-                    reqParamsCopy.where.populate = [reqParamsCopy.where.populate,reqParamsCopy.populate]
-                }
-                else if(Array.isArray(reqParamsCopy.populate) && !reqParamsCopy.populate.includes(reqParamsCopy.where.populate)){
-                    reqParamsCopy.where.populate = [reqParamsCopy.where.populate, ...reqParamsCopy.populate]
-                }
-            }
-            reqParamsCopy = { ...reqParamsCopy, ...reqParamsCopy.where }
-        }
+
+        paramsMergeIntoWhere(reqParamsCopy)
+
         let paramsWithoutKeywords = removeKeywords(reqParamsCopy) //limit,populate,projection,skip,sort,where
+        
         var query = services.Api[modelName].find({...paramsWithoutKeywords},null)
-        operations(query,reqParamsCopy)
+        cursorOperations(query,reqParamsCopy)
         .then((data)=>{
             if(options && options.hasNext){
                 res.locals.data = data
@@ -38,5 +30,20 @@ module.exports = function(options){
         .catch((error)=>{
             res.status(500).json(error)
         })
+    }
+}
+
+function paramsMergeIntoWhere(reqParamsCopy){
+    if(reqParamsCopy && reqParamsCopy.where){
+        reqParamsCopy.where = JSON.parse(reqParamsCopy.where)
+        if(reqParamsCopy.populate && reqParamsCopy.where.populate){
+            if(typeof(reqParamsCopy.populate)=="string" && reqParamsCopy.populate != reqParamsCopy.where.populate){
+                reqParamsCopy.where.populate = [reqParamsCopy.where.populate,reqParamsCopy.populate]
+            }
+            else if(Array.isArray(reqParamsCopy.populate) && !reqParamsCopy.populate.includes(reqParamsCopy.where.populate)){
+                reqParamsCopy.where.populate = [reqParamsCopy.where.populate, ...reqParamsCopy.populate]
+            }
+        }
+        reqParamsCopy = { ...reqParamsCopy, ...reqParamsCopy.where }
     }
 }
