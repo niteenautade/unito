8438a05dc26d9225d5ad8791af9326479e257c09
niteenautade <niteenautade@gmail.com>
2018-05-06 17:29:11 +0530

    custom subroutes app.get to app.all

--- routes/subRoutes.js
+++ routes/subRoutes.js
@@ -2,7 +2,7 @@ module.exports = function(app,controllers,key,routeName,services){
     var newRoutes = Object.keys(controllers[key]).filter(subRouteName => !["find","findOne","create","update","destroy"].includes(subRouteName))
     if(newRoutes.length > 0){
         newRoutes.forEach(newRoute => {
-            app['get']('/'+routeName+'/'+newRoute,
+            app['all']('/'+routeName+'/'+newRoute,
                 (req,res,next)=>{
                     services.aggregateParams(req)
                     req.models = app.models
