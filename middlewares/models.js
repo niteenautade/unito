2f9559bdd5a0a1c790b56481b10f41f473121cf2
niteenautade <niteenautade@gmail.com>
2018-05-13 00:09:55 +0530

    subdocument handling in schema

--- middlewares/models.js
+++ middlewares/models.js
@@ -9,7 +9,54 @@ module.exports = function(mongoose,modelSchemas,config){
                 modelSchema.attributes.properties[key] = { type: mongoose.Schema.Types.ObjectId, ref: modelSchema.reference[key].model }
             })
         }
-        var options = {collection:name,versionKey: false,timestamps:true }
+        
+        
+        Object.keys(modelSchema.attributes.properties)
+        .forEach(modelSchemaProperty =>{
+            var property = modelSchema.attributes.properties[modelSchemaProperty]
+            
+            let options = {_id : false}
+            if(property && !property.hasOwnProperty("additionalProperties")){
+                options.strict = false
+            }
+            else if(property && property.hasOwnProperty("additionalProperties")){
+                options.strict = !property.additionalProperties            
+            }
+
+            if(property && typeof(property) == "object" && property.hasOwnProperty("type") && (property.type == "object" || property.type == "Object") && !Array.isArray(property)){
+                var subDocumentSchema = new mongoose.Schema(property.properties,options)
+                modelSchema.attributes.properties[modelSchemaProperty]["type"] = subDocumentSchema
+            }
+
+            // console.log(modelSchemaProperty,options)
+            
+            if(property && typeof(property) == "object" && property.hasOwnProperty("type") && (property.type == "array" || property.type == "Array")  && !Array.isArray(property)&& property.items && (property.items.type == "object" || property.items.type == "Object")){
+                options = {_id : false}
+                if(property && !property.items.hasOwnProperty("additionalProperties")){
+                    options.strict = false
+                }
+                else if(property && property.items.hasOwnProperty("additionalProperties")){
+                    options.strict = !property.additionalProperties            
+                }
+                var subDocumentSchema = new mongoose.Schema(property.items.properties,options)
+                modelSchema.attributes.properties[modelSchemaProperty]["type"] = [subDocumentSchema]
+            }
+
+            else if(property && typeof(property) == "object" && property.hasOwnProperty("type") && (property.type == "array" || property.type == "Array")  && !Array.isArray(property)&& property.items){
+                options = {_id : false}
+                if(property && !property.items.hasOwnProperty("additionalProperties")){
+                    options.strict = false
+                }
+                else if(property && property.items.hasOwnProperty("additionalProperties")){
+                    options.strict = !property.additionalProperties            
+                }
+                var subDocumentSchema = new mongoose.Schema(property.items.properties,options)
+                modelSchema.attributes.properties[modelSchemaProperty]["type"] = [subDocumentSchema]
+            }
+
+        })
+        // console.log(JSON.stringify(modelSchema,null,4))
+        let options = {collection:name,versionKey: false,timestamps:true }
         if(!modelSchema.attributes.hasOwnProperty("additionalProperties")){
             options.strict = false
         }
