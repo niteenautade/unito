3a615efdef1612ef0df3d05d5eb6a76cbb90765f
niteenautade <niteenautade@gmail.com>
2018-07-14 01:08:53 +0530

    cmdline init

--- services/cmdline/index.js
+++ services/cmdline/index.js
@@ -5,16 +5,34 @@ var path = require("path")
 var fs = require('fs')
 var config = require('./config')()
 
-var keys = Object.keys(config)
-keys.forEach(key=>{
-    if(argv[key]){
-        var template = require(path.resolve(__dirname,"templates/"+key))(argv[key])
-        
-        var suffix = config[key]["suffix"] ? config[key]["suffix"] : ""
-        var filename = argv[key][0].toUpperCase()+argv[key].substr(1).toLowerCase()+suffix
-        fs.writeFile(path.resolve(process.cwd(),config[key].output,filename), template, (err) => {
-            if (err) throw err;
-            console.log("Created at "+path.resolve(process.cwd(),config[key].output,filename));
-        });
+function createDirectory(path){
+    if (!fs.existsSync(path)){
+        fs.mkdirSync(path);
     }
-})
+}
+
+if(argv["_"][0]==="init"){
+    var arr = [
+        "api",
+        "api/controllers",
+        "api/models",
+        "api/services",
+        "config"
+
+    ]
+}
+else{
+    var keys = Object.keys(config)
+    keys.forEach(key=>{
+        if(argv[key]){
+            var template = require(path.resolve(__dirname,"templates/"+key))(argv[key])
+    
+            var suffix = config[key]["suffix"] ? config[key]["suffix"] : ""
+            var filename = argv[key][0].toUpperCase()+argv[key].substr(1).toLowerCase()+suffix
+            fs.writeFile(path.resolve(process.cwd(),config[key].output,filename), template, (err) => {
+                if (err) throw err;
+                console.log("Created at "+path.resolve(process.cwd(),config[key].output,filename));
+            });
+        }
+    })
+}
