5d96ceb1f0659c7bac0e0b0e104c11f197e92292
niteenautade <niteenautade@gmail.com>
2018-07-14 01:41:02 +0530

    cmdline init

--- services/cmdline/templates/package.js
+++ services/cmdline/templates/package.js
@@ -18,22 +18,3 @@ module.exports = function(){
   }`
     return template
     }
-
-
-
-{
-    "name": "frameworkapp",
-    "version": "1.0.0",
-    "description": "",
-    "main": "index.js",
-    "scripts": {
-      "start": "node app.js",
-      "test": "echo \"Error: no test specified\" && exit 1"
-    },
-    "dependencies": {
-      "unito": "^1.0.0"
-    },
-    "author": "",
-    "license": "ISC"
-  }
-  
