1daf2ee4b55e9e13820ed81e7c5736546d3c53e5
niteenautade <niteenautade@gmail.com>
2018-07-14 01:00:18 +0530

    cmdline template updat

--- services/cmdline/templates/controller.js
+++ services/cmdline/templates/controller.js
@@ -4,6 +4,7 @@ var template =
 Controller : ${name[0].toUpperCase()}${name.substr(1).toLowerCase()}
 --------------------------------------------------------------------------*/
 var blueprints = require("unito/middlewares/blueprints")()
+var Token = require("unito/services/token")
 var Api = require("unito/api")
 
 module.exports = {
