024d2f70b0ca40c9b4de20d9285cf0bbd43d5e59
niteenautade <niteenautade@gmail.com>
2018-07-14 15:29:02 +0530

    added acl template in cmdline tool

---
+++ services/cmdline/templates/acl.js
@@ -0,0 +1,17 @@
+module.exports = function(){
+    var template =
+`module.exports = {
+ /*   "unauthenticated": {
+                            "User" : {
+                                find : true,
+                                findOne : true,
+                                create : true,
+                                update : true,
+                                destroy : true
+                                }
+                        }
+    */
+}`
+    return template
+    }
+
