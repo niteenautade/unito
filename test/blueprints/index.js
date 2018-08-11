ba52a063d30dde9c8d7edb753449acc134b66ccc
niteenautade <niteenautade@gmail.com>
2018-08-11 19:00:43 +0530

    blueprints test

--- test/blueprints/index.js
+++ test/blueprints/index.js
@@ -185,6 +185,27 @@ describe('Testing blueprints', () => {
         })
     });
 
+    
+    it('Delete a user', (done) => {
+        let data = {"name":"Niteen Autade"}
+        chai.request(server)
+        .post('/user')
+        .send(data)
+        .end((err, res) => {
+            res.should.have.status(201);
+            res.body.should.be.a('object');
+            res.body.name.should.be.eql("Niteen Autade");
+            chai.expect(res.body).to.have.property("id")
+            return chai.request(server)
+            .delete('/user/'+res.body.id)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                done();
+            });
+
+        })
+    });
     it('Check if posting date gets stored stored as date object in db', (done) => {
 
         let data = {"testDate":new Date().toISOString(),name:"abc"} 
@@ -205,6 +226,29 @@ describe('Testing blueprints', () => {
                     done()
                 })
         });
+    });
+    
+    it('id is received in response but stored as _id', (done) => {
+        let data = {"name":"Niteen Autade"} 
+        chai.request(server)
+            .post('/user')
+            .send(data)
+            .end((err, res) => {
+                res.should.have.status(201);
+                res.body.should.be.a('object');
+                res.body.name.should.be.eql(data.name)
+                chai.expect(res.body).to.have.property("id")                
+                User.findOne({_id:res.body.id})
+                .then(user=>{
+                    chai.expect(user).to.have.property("_id")
+                    user._id.should.be.a('object')
+                    done()
+                })
+                .catch(err=>{
+                    console.log(err)
+                    done()
+                })
+            });
     });
 
 
