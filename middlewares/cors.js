8555984227211d520249cef68ccf3e64307292b2
Niteen Autade <niteen@localhost.localdomain>
2018-09-26 20:50:04 +0530

    cors fix

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,15 +3,8 @@ var findUp = require("find-up")
 var corsConfigPath = findUp.sync('./config/cors.js')
 var corsConfig = require(corsConfigPath)
 
-var whitelist = !corsConfig ? [] : corsConfig.whitelist
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
+if(corsConfig){
+	console.log(corsConfig)
+	
 }
-
-module.exports = cors(corsOptions)
+module.exports = cors(corsConfig)
