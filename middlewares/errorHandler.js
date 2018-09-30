e0dd5dbd2454ea1af1d34dac520e8ea8d1efdd14
Niteen Autade <niteen@localhost.localdomain>
2018-09-30 17:41:42 +0530

    validationError in mongoose with status 400

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -3,6 +3,9 @@ module.exports = function(error,req,res,next){
 	if(error.hasOwnProperty("status")){
 		return res.status(error.status).json(error)
 	}
+	else if(error.name == "ValidationError"){
+		return res.status(400).json(error)
+	}
 	else{
 		return res.status(500).json(error)
 	}
